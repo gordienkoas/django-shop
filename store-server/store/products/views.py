666590aee6b9d83f8ec2903d89f92ea91d85010a
user <gordienkoas@bk.ru>
2023-12-03 16:11:01 +0300

    models

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+               'title': 'Store',
+               'is_promotion': False,
+               }
+    return render(request, 'products/index.html', context)
 
 def products(request):
     return render(request, 'products/products.html')
