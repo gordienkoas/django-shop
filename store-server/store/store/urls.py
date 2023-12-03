4ad9fbf101a8eae313d9f4462c4d2b2d3885263b
user <gordienkoas@bk.ru>
2023-12-03 18:28:52 +0300

    auth model

--- store-server/store/store/urls.py
+++ store-server/store/store/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
 ]
