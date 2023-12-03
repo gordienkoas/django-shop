9559fdf2f209a7fc97998470208458e04260a9cd
user <gordienkoas@bk.ru>
2023-12-03 19:37:20 +0300

    auth forms

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
 def login(request):
-    return render(request, 'users/login.html')
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
     return render(request, 'users/registration.html')
